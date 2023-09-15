008b1241a91b756b57806e5fbc77f2490dfbdefb
Timon <timon@tsharp.com>
2023-09-15 09:20:58 +0000

    initial concept for MemoRelativeSetR

--- MemoizR/MemoReducR.cs
+++ MemoizR/MemoReducR.cs
@@ -2,36 +2,13 @@ namespace MemoizR;
 
 public sealed class MemoReducR<T> : MemoHandlR<T> // ToDo: , IMemoizR
 {
-    private List<Func<T, T>> reducers = new List<Func<T, T>>();
-
-
-    internal MemoReducR(Context context, string label = "Label") : base(context, null)
+    internal MemoReducR(Func<T, T> reduce, Context context, string label = "Label") : base(context, null)
     {
         this.label = label;
     }
 
-    public void Set(Func<T, T> reduce)
-    {
-        lock (context)
-        {
-            for (int i = 0; i < Observers.Length; i++)
-            {
-                var observer = Observers[i];
-                observer.Stale(CacheState.CacheDirty);
-            }
-
-            reducers.Add(reduce);
-        }
-    }
-
     public T? Get()
     {
-        if (!reducers.Any())
-        {
-            return value;
-        }
-
-
         if (context.CurrentReaction == null) // ToDo: State == CacheState.CacheClean
         {
             return value;
@@ -51,12 +28,7 @@ public sealed class MemoReducR<T> : MemoHandlR<T> // ToDo: , IMemoizR
                 if (!context.CurrentGets!.Any()) context.CurrentGets = new[] { this };
                 else context.CurrentGets = context.CurrentGets!.Union(new[] { this }).ToArray();
             }
-
-            foreach (var reducer in reducers)
-            {
-                value = reducer(value!);
-            }
-            reducers.Clear();
+            
         }
 
         return value;
